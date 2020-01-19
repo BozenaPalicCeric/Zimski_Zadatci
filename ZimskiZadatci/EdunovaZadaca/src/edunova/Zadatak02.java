7dcb2d90626d01efe0ce881d138e217b9ba11ce7
BozenaPalicCeric <bozena.palic@gmail.com>
2020-01-19 23:35:56 +0100

    Create Zadatak02.java

---
+++ ZimskiZadatci/EdunovaZadaca/src/edunova/Zadatak02.java
@@ -0,0 +1,17 @@
+package edunova;
+
+public class Zadatak02 {
+
+	public static void main(String[] args) {
+		// Kreirati program koji ispisuje tablicu množenja 15x15
+		
+		for (int i=0; i<15;i++) {
+			for (int j=0; j<15; j++) {
+				System.out.printf("%4d",((i+1)*(j+1)));
+			}
+			System.out.println();
+		}
+
+	}
+
+}
